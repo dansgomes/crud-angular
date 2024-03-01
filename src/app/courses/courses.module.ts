5cfd64f77ee46298a0c6ac8fa3e4cb55e2aef500
Daniel <danielsgomes99@gmail.com>
2024-03-01 14:57:58 -0300

    Criando Material  Tables para listar cursos, dando valores de "_id", "name" e "category".

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
 import { CoursesRoutingModule } from './courses-routing.module';
 
 
@@ -8,7 +9,9 @@ import { CoursesRoutingModule } from './courses-routing.module';
   declarations: [],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatToolbarModule,
+    MatTableModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTableModule} from '@angular/material/table';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category'];
 }
